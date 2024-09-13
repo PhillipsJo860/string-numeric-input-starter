29d2f4067fb4c33024681fab649d3a16c5f17019
SoggyCereal <PhillipsJo860@northwested.org>
2024-09-13 11:04:15 -0400

    added content

--- main.py
+++ main.py
@@ -1,44 +1,38 @@
-# Student name
-# Current date
+# Joshua Phillips
+# 9/13/24
 # String & Numeric Input 
 
 # Task 1
 # Comment
 # Comment
-
-
-
+# CODE STARTS HERE:
+# name = input('Please enter youre first and last name: ')
+# program = input('Next, please enter the program which you attendat CTC: ')
+# print('Hello ' + name + ', are you enjoying ' + program + '?')
 
 # Task 2
 # Comment
 # Comment
+# CODE STARTS HERE:
+# age1 = input('How old are you? fairly old right?(ENTER YOR AGE HERE): ')
+# int(age1)
 
-
-
+# print(f'Your age is {age1}!')
 
 # Task 3
 # Comment
-# Comment
-
-
-
+# Commentnumber
+# CODE STARTS HERE:
+# num_of_fam = int(input('how many family numbers do you have?: '))
+# print('you have '+str(num_of_fam)+' family members!')
+# print(f'You have {num_of_fam} members in ur fam.')
 
 # Task 4
+# Comment 
 # Comment
-# Comment
+# CODE STARS HERE:
+height_inches = int(input('enter your height in inches to see it in centimeters: '))
+height_centi = height_inches * 2.54
 
-
-
-
-
-# Task 5
-# Comment
-# Comment
-
-
-
-
-
-# Part 5
-# Comment
-# Comment
+print('Your height in centimeters is '+str(height_centi))+'!'
+print(f'Your height in centimeters is {height_centi}!')
